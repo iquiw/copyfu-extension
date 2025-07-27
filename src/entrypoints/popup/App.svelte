c173dc4dbc539fc56dfba62933e51fda0774ac61
Iku Iwasa <iku.iwasa@gmail.com>
2025-07-27 14:54:33 +0900

    feat: Change Options button text to Settings icon

--- src/entrypoints/popup/App.svelte
+++ src/entrypoints/popup/App.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
+  import { Settings } from '@lucide/svelte';
+  import appIcon from '../../assets/copyfu.svg';
+
   import Format from './Format.svelte';
 </script>
 
 <main>
   <div class="grid grid-cols-1 gap-2 m-4">
     <Format />
-    <button type="button" class="btn preset-filled-surface-500" onclick={() => browser.runtime.openOptionsPage()}>Options</button>
+    <div class="flex justify-end">
+      <img src={appIcon} class="logo" alt="CopyFU Logo" />
+      <h4 class="h4">CopyFU</h4>
+      <span class="grow"></span>
+      <button type="button" class="btn options" title="Options" onclick={() => browser.runtime.openOptionsPage()}>
+        <Settings size="24" />
+      </button>
+    </div>
   </div>
 </main>
+
+<style>
+  .logo {
+    margin: 0.5rem;
+    height: 1.5rem;
+  }
+  .h4 {
+    height: 2.25rem;
+    line-height: 2.25rem;
+  }
+</style>
