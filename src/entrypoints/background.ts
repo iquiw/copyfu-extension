00ca9f9e90fa424423b71a6b4f8cf3c15d2d4f40
Iku Iwasa <iku.iwasa@gmail.com>
2025-08-30 22:54:47 +0900

    feat(background): Install preset when no template exists

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,14 +1,23 @@
+import { loadTemplates, saveTemplates } from "@/lib/storage";
+
 function sendCopyMessage(index: number) {
   setTimeout(() => {
     browser.runtime.sendMessage({ action: 'copy-template', index });
   }, 300);
 }
 
-const commandRegexp = new RegExp('copy-template-(\\d+)');
+const CommandRegexp = new RegExp('copy-template-(\\d+)');
+
+const TemplatesPreset = [
+  { name: 'Markdown', template: '[{{title}}]({{url}})' },
+  { name: 'Org Mode', template: '[[{{url}}][{{title}}]]' },
+  { name: 'AsciiDoc', template: '{{url}}[{{title}}]' },
+  { name: 'Feed Link', template: '{{ feeds | first | map: "url" }}' },
+];
 
 export default defineBackground(() => {
   browser.commands.onCommand.addListener((command) => {
-    const match = commandRegexp.exec(command);
+    const match = CommandRegexp.exec(command);
     if (match) {
       const index = parseInt(match[1], 10);
       (browser.action ?? browser.browserAction).openPopup().then(() => {
@@ -23,5 +32,13 @@ export default defineBackground(() => {
         }
       });
     }
+  });
+
+  browser.runtime.onInstalled.addListener((details) => {
+    loadTemplates().then((ftempls) => {
+      if (ftempls.length == 0) {
+        saveTemplates(TemplatesPreset);
+      }
+    });
   });
 });
